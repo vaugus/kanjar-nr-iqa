1aafbad5ff3aed4c775fdfe3e4694de33911f622
vaugusto92 <vaugusto92@outlook.com>
2021-10-31 20:14:49 -0300

    Added a new core file to be implemented.

---
+++ kanjar/core.py
@@ -0,0 +1,64 @@
+from abc import ABC, abstractmethod
+
+import os
+import sys
+
+import numpy as np
+
+from scipy.stats import kurtosis
+
+import pywt
+import imageio
+import cv2
+from termcolor import colored
+
+class Kanjar(ABC):
+
+    def template_method(self):
+
+        images = [] 
+
+        root = ''
+        path = ''
+
+        for name in images:
+            print (colored('Computing image ' + str(name) + '...', 'red'))
+            arr = []
+            
+            for j in ['1', '2', '3', '4', '5']:
+                img = imageio.imread(path + name + '.BLUR.' + j + '.png')
+
+                fft = np.fft.fftshift(np.fft.fft2(img))
+
+                # compute the absolute value of all Fourier coefficients
+                abs_val = np.abs(fft)
+
+                # compute the maximum value among all coefficients
+                M = np.max(abs_val)
+
+                # compute the total number of coefficients that are higher than
+                # the maximum value / 1000
+                Th = abs_val[abs_val > M / 1000].size
+
+                print(Th / img.size)
+
+            np.savetxt(root + name + '-kanjar-.txt', arr, fmt='%.10f')
+
+
+
+    def base_operation1(self):
+        pass
+
+    def base_operation2(self):
+        pass
+
+    def base_operation3(self):
+        pass
+
+    @abstractmethod
+    def required_operations1(self):
+        pass
+
+    @abstractmethod
+    def required_operations2(self):
+        pass
